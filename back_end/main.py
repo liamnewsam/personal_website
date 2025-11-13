1c66e154a06b7dcbda85185320a67c5910987b23
Liam Newsam <liamcnewsam@gmail.com>
2025-11-12 20:53:27 -0800

    adding try except clause

--- back_end/main.py
+++ back_end/main.py
@@ -4,16 +4,19 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS  # NEW
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app)
 
 @app.route("/digit-predict", methods=["POST", "OPTIONS"])
 def handle_digit():
     if request.method == "OPTIONS":
         return '', 200
-    data = request.get_json()  # Expecting JSON data
-    image = data.get("image")
-    result = predict_digit(image)
-    return jsonify(result)
-
+    try:
+        data = request.get_json()
+        image = data.get("image")
+        result = predict_digit(image)
+        return jsonify(result)
+    except Exception as e:
+        print("Error in /digit-predict:", e)
+        return jsonify({"error": str(e)}), 500
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
