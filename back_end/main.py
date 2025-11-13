c4fb2712baec8fd47e5f2a147f8088cbf15a78e9
Liam Newsam <liamcnewsam@gmail.com>
2025-11-13 15:13:57 -0800

    need to start main

--- back_end/main.py
+++ back_end/main.py
@@ -26,4 +26,7 @@ def predict():
         print("Error in /digit-predict:", e)
         return jsonify({"error": str(e)}), 500
 
-
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    print(f"Running on port {port}...")
+    app.run(host="0.0.0.0", port=port)
