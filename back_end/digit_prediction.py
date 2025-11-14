1d3ed3b9f6c8a35280ccd54181f90a6cff40c8de
Liam Newsam <liamcnewsam@gmail.com>
2025-11-14 13:11:08 -0800

    lets see

--- back_end/digit_prediction.py
+++ back_end/digit_prediction.py
@@ -21,7 +21,7 @@ def predict_digit(image_base64):
     arr = arr.reshape(1, 1, 28, 28)
 
     outputs = session.run(None, {input_name: arr})
-    logits = outputs[0]
+    logits = outputs[0][0]
     print(logits)
     exp = np.exp(logits - np.max(logits))
     probs = exp / exp.sum()
